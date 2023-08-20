d634f753149a30b13906abb897e5907cd68d5a24
Volodymyr Popov <texnolife@gmail.com>
2023-08-21 00:28:54 +0100

    add modal window plus js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
-console.log('Conected');
+const howToPlayLink = document.querySelector(".modal-lnk");
+const modal = document.querySelector(".modal");
+const gotItBtn = document.querySelector(".got-it-btn");
+
+/* ---  Modal How to play ---*/
+
+howToPlayLink.onclick = () => {
+    modal.classList.add("active");
+  };
+
+  gotItBtn.onclick = () => {
+    modal.classList.remove("active");
+  };
+  /* --- END Modal How to play ---*/
+  
