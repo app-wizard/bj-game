7f8db2e8cc9fe05181fd05ee680271c7320019ed
Volodymyr Popov <texnolife@gmail.com>
2023-08-21 01:03:45 +0100

    add game saction

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,22 @@
 const howToPlayLink = document.querySelector(".modal-lnk");
 const modal = document.querySelector(".modal");
 const gotItBtn = document.querySelector(".got-it-btn");
+const newGameBtn = document.querySelector(".new-game-btn");
+const gameSection = document.querySelector(".game-section");
 
 /* ---  Modal How to play ---*/
-
 howToPlayLink.onclick = () => {
-    modal.classList.add("active");
-  };
+  modal.classList.add("active");
+};
 
-  gotItBtn.onclick = () => {
-    modal.classList.remove("active");
-  };
-  /* --- END Modal How to play ---*/
-  
+gotItBtn.onclick = () => {
+  modal.classList.remove("active");
+};
+/* --- END Modal How to play ---*/
+
+
+/* ---  New Game ---*/
+newGameBtn.onclick = () => {
+  gameSection.classList.add("active");
+};
+/* --- END New Game ---*/
