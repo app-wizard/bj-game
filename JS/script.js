7d1865fcfae813280f2ca7ca8796ff1a57bca026
Volodymyr Popov <texnolife@gmail.com>
2023-08-21 11:21:29 +0100

    add start game function

--- JS/script.js
+++ JS/script.js
@@ -14,9 +14,45 @@ gotItBtn.onclick = () => {
 };
 /* --- END Modal How to play ---*/
 
-
 /* ---  New Game ---*/
 newGameBtn.onclick = () => {
   gameSection.classList.add("active");
+  startNewGame();
+};
+
+ function startNewGame () {
+  buildCards();
 };
 /* --- END New Game ---*/
+
+/* ---  Game Section ---*/
+
+function buildCards(){
+  let value = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  let types = ["C", "D", "H", "S"];
+  let cards = [];
+
+  value.forEach((v) => {
+    types.forEach((t) => {
+      cards.push(v + "-" + t);
+    });
+  });
+
+  console.log(cards);
+};
+
+/* ---  END Game Section ---*/
